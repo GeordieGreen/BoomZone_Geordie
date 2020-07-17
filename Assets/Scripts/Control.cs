98e8eb16db9b3509a8f6e278b62300a6bc393f5d
GeordieGreen <68436694+GeordieGreen@users.noreply.github.com>
2020-07-17 08:57:17 -0700

    Initial commit

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0,20f,0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -20f, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(20f, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(-20f, 0, 0) * Time.deltaTime);
+        }
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, -70, 70);
+        position.z = Mathf.Clamp(position.z, -70, 70);
+
+        transform.position = position;
+    }
+
+    
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+
+{
+    public GameObject burst;
+    public GameObject particle;
+
+    public Transform playerPosition;
+    public ParticleSystem explosion;
+
+    private void Start()
+    {
+        explosion.Stop();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            burst.SetActive(true);
+        }
+        else
+        {
+            burst.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(particle, playerPosition.position, Quaternion.identity);
+        }
+    }
+}
